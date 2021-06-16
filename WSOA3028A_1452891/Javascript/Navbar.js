f5abfd4a606e4768e6076cdbdf27b5264415f612
Justin-Perumal <1452891@students.wits.ac.za>
2021-06-16 21:52:35 +0200

    Navbar created through JS
    All pages for the website will now have the navbar created dynamically using JavaScript

--- WSOA3028A_1452891/Javascript/Navbar.js
+++ WSOA3028A_1452891/Javascript/Navbar.js
@@ -1,12 +1,113 @@
-/*var BlogPosts = [
-    {title: "Readings from Jorge Luis Borges", date: new Date('2021/03/12'), href: './Blogs/Theory Blogs/BLOG1/Blog 1.html'},
-    {title: "Promise and peril of UGC", date: new Date('2021/03/19'), href: './Blogs/Theory Blogs/BLOG2/Blog 2.html'},
+var TheoryBlogPosts = [
+    {title: "Readings from Jorge Luis Borges.", date: new Date('2021/03/12'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG1/Blog 1.html'},
+    {title: "Promise and peril of UGC.", date: new Date('2021/03/19'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG2/Blog 2.html'},
+    {title: "Semantic Markup?", date: new Date('2021/03/26'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG3/Blog 3.html'},
+    {title: "Medium is the Message.", date: new Date('2021/04/02'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG4/Blog 4.html'},
+    {title: "Importance of Accessibility.", date: new Date('2021/04/09'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG5/Blog 5.html'},
+    {title: "Tools I use for Web Development.", date: new Date('2021/04/09'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BONUS BLOG 1/Bonus Blog 1.html'},
+    {title: "Norman Doors and Web Design?", date: new Date('2021/04/16'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG6/Blog 6.html'},
+    {title: "Usability on the Internet!", date: new Date('2021/04/23'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG7/Blog 7.html'},
+    {title: "The Digital Divide!", date: new Date('2021/04/30'), href: '/WSOA3028A_1452891/Blogs/Theory Blogs/BLOG8/Blog 8.html'},
 ]
 
-To implement dynamic navbar and navigation --> a W.I.P
-*/
+var CharacterBlogPosts = [
+    {title: "Wireframes and Design", date: new Date('2021/03/26'), href: '/WSOA3028A_1452891/Blogs/Character Blogs/Wireframes and Design/Wireframes.html'},
+    {title: "Character description and Rationale", date: new Date('2021/03/25'), href: '/WSOA3028A_1452891/Blogs/Character Blogs/Character Rationale/Rationale.html'},
+    {title: "Website Style Guide", date: new Date('2021/05/22'), href: '/WSOA3028A_1452891/Blogs/Character Blogs/Website Style Guide/StyleGuide.html'},
+    {title: "Sous Vide", date: new Date('2021/04/12'), href: '/WSOA3028A_1452891/Blogs/Character Blogs/Blog 1/Character Blog 1.html'},
+]
+
+var NavItems = [
+    {item: 'Home', href: '/WSOA3028A_1452891/index.html'},
+    {item: 'Recipes', href: '/WSOA3028A_1452891/Recipes/RecipesMain.html'},
+    {item: 'Restaurants', href: '/WSOA3028A_1452891/Restaurants/RestaurantsMain.html'},
+    {item: 'Theory Blogs', href: '/WSOA3028A_1452891/Blogs/Theory Blogs/TheoryBlogMain.html'},
+    {item: 'Character Blogs', href: '/WSOA3028A_1452891/Blogs/Character Blogs/CharacterBlogsMain.html'},
+    {item: 'About', href: '/WSOA3028A_1452891/About and Contact info/About.html'}
+]
+
+const newNavBar = document.createElement('nav');
+const NavLocation = document.getElementById('NavLocation');
+const NewNavList = document.createElement('ul');
+
+function CreateNav()
+{
+    console.log("creating nav");
+
+    let NavLength = NavItems.length;
+    for(var i=0; i< NavLength; i++)
+    {
+        let NewNavItem = document.createElement('li');
+        let NavItemLink = document.createElement('a');
+        NavItemLink.href = NavItems[i].href;
+        NavItemLink.innerHTML = NavItems[i].item;
+        if(NavItems[i].item == "Theory Blogs")
+        {
+            NewNavItem.id = "BlogDropdown";
+            CreateTheoryBlogs();
+            NewNavItem.appendChild(TheoryBlogList)
+        }
+        else if(NavItems[i].item == "Character Blogs")
+        {
+            NewNavItem.id = "CharacterDropdown";
+            CreateCharacterBlogs();
+            NewNavItem.appendChild(CharacterBlogList);
+        }
+        NewNavItem.appendChild(NavItemLink);
+        console.log('adding item ' + NavItems[i].item);
+        NewNavList.appendChild(NewNavItem);
+    }
+
+    newNavBar.appendChild(NewNavList);
+    NavLocation.appendChild(newNavBar);
+
+}
+
+const TheoryBlogList = document.createElement('ul')
+
+function CreateTheoryBlogs()
+{
+    console.log("creating theory blogs");
+    TheoryBlogList.id = "Dropdown";
+
+    let TheoryLength = TheoryBlogPosts.length;
+    for(var i=0; i<TheoryLength; i++)
+    {
+        let NewTheoryItem = document.createElement('li');
+        let TheoryLink = document.createElement('a');
+        TheoryLink.href = TheoryBlogPosts[i].href;
+        TheoryLink.innerHTML = TheoryBlogPosts[i].title;
+        NewTheoryItem.appendChild(TheoryLink);
+        console.log("creating theory blog " + TheoryBlogPosts[i].title);
+        TheoryBlogList.appendChild(NewTheoryItem);
+    }
+}
+
+const CharacterBlogList = document.createElement('ul');
+
+function CreateCharacterBlogs()
+{
+    console.log("creating character blogs");
+    CharacterBlogList.id = "DropdownCharacter";
+
+    let CharacterLength = CharacterBlogPosts.length
+    for(var i=0; i<CharacterLength; i++)
+    {
+        let NewCharacterItem = document.createElement('li');
+        let CharacterLink = document.createElement('a');
+        CharacterLink.href = CharacterBlogPosts[i].href;
+        CharacterLink.innerHTML = CharacterBlogPosts[i].title;
+        NewCharacterItem.appendChild(CharacterLink);
+        console.log("Creating character blog " + CharacterBlogPosts[i].title);
+        CharacterBlogList.appendChild(NewCharacterItem);
+    }
+}
 
 //Dropdown control for theory blogs
+
+CreateNav();
+console.log("Page loaded");
+
 document.getElementById("BlogDropdown").addEventListener("mouseenter", OpenMenuTheory);
 document.getElementById("BlogDropdown").addEventListener("mouseleave", CloseMenuTheory);
 
@@ -22,6 +123,7 @@ function CloseMenuTheory()
     document.getElementById("BlogDropdown").classList.add("DropdownInactive");
 }
 
+
 //Dropdown control for character blogs
 document.getElementById("CharacterDropdown").addEventListener("mouseenter", OpenMenuCharacter);
 document.getElementById("CharacterDropdown").addEventListener("mouseleave", CloseMenuCharacter);
@@ -36,5 +138,6 @@ function CloseMenuCharacter()
     console.log("close");
     document.getElementById("DropdownCharacter").classList.remove("DropdownActive");
     document.getElementById("CharacterDropdown").classList.add("DropdownInactive");
-}
+} 
+
 
